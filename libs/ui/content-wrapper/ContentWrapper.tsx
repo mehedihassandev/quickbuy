450b55699c04bd1cc5d3cbc9b748b6a8137f834c
Amit Roy <amitroysunjoy@gmail.com>
2024-04-25 16:32:09 +0600

    QB-4: ContentWrapper created

---
+++ libs/ui/content-wrapper/ContentWrapper.tsx
@@ -0,0 +1,29 @@
+import { Box, SxProps } from "@mui/material";
+import React, { CSSProperties, ReactNode } from "react";
+
+export interface ContentWrapperProps {
+  children: ReactNode;
+  style?: CSSProperties;
+  sx?: SxProps;
+}
+
+export const ContentWrapper = ({
+  children,
+  style,
+  sx,
+}: ContentWrapperProps) => {
+  return (
+    <Box
+      sx={{
+        bgcolor: "white",
+        padding: 2.5,
+        ...style,
+        ...sx,
+      }}
+    >
+      <>{children}</>
+    </Box>
+  );
+};
+
+export default ContentWrapper;
